56f3ded0212da428e2c3678ef810c2441282a55d
phungmai99 <87408756+phungmai99@users.noreply.github.com>
2021-07-22 16:48:16 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function clickViewInfoRow1() {
+    location.href = "https://www.facebook.com/shruti.t.gopi";
+}
+function clickViewMoreRow2(){
+    location.href = "https://phungmai99.github.io/Me.github.io/";
+}
+function clickViewMoreRow3(){
+    location.href = "https://phungmai99.github.io/TheMozStory.github.io/index.html";
+}
+function clickViewMoreRow4(){
+    location.href = "https://phungmai99.github.io/FinalWeb.github/index.html";
+}
